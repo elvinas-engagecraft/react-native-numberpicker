835c62451b71d35828c138271a04e51f883108e3
Elvinas <elvinas@engagecraft.com>
2019-06-10 15:44:55 +0300

    RN 0.59.x

--- src/main/java/io/fixd/reactnativenumberpicker/RNNumberPickerPackage.java
+++ src/main/java/io/fixd/reactnativenumberpicker/RNNumberPickerPackage.java
@@ -1,29 +1,24 @@
 package io.fixd.reactnativenumberpicker;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RNNumberPickerPackage implements ReactPackage {
 
+    @NonNull
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) { return Collections.emptyList(); }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
+    @NonNull
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Arrays.<ViewManager>asList(
-            new RNNumberPickerManager()
-        );
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.<ViewManager>singletonList(new RNNumberPickerManager());
     }
 }
