ec896ccc89408ee1b8618776e0c09981120ee584
Elvinas <elvinas@engagecraft.com>
2019-08-02 12:44:36 +0300

    Fix initial value setting
    For some reason setValue fires before setValues, so stage the selection until we have the values.

--- src/main/java/io/fixd/reactnativenumberpicker/RNNumberPickerManager.java
+++ src/main/java/io/fixd/reactnativenumberpicker/RNNumberPickerManager.java
@@ -1,62 +1,76 @@
 package io.fixd.reactnativenumberpicker;
 
-import javax.annotation.Nullable;
+import android.support.annotation.NonNull;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.events.EventDispatcher;
-import com.facebook.react.uimanager.UIManagerModule;
-import com.facebook.react.common.SystemClock;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.uimanager.ViewProps;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 
-import java.lang.Integer;
-import java.lang.String;
+import javax.annotation.Nullable;
 
 public class RNNumberPickerManager extends SimpleViewManager<RNNumberPicker>
         implements RNNumberPicker.OnChangeListener {
 
-    public static final String REACT_CLASS = "RNNumberPicker";
+    private static final String REACT_CLASS = "RNNumberPicker";
 
+    @NonNull
     @Override
     public String getName() {
         return REACT_CLASS;
     }
 
+    @NonNull
     @Override
-    protected RNNumberPicker createViewInstance(ThemedReactContext reactContext) {
+    protected RNNumberPicker createViewInstance(@NonNull ThemedReactContext reactContext) {
         return new RNNumberPicker(reactContext);
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = "values")
-    public void setValues(RNNumberPicker view, @Nullable  ReadableArray items) {
-        String[] displayValues = new String[items.size()];
-        for (int i = 0; i < items.size(); i++) {
-            displayValues[i] = (String) items.getString(i);
+    public void setValues(RNNumberPicker view, @Nullable ReadableArray items) {
+        boolean updateStaged = view.getDisplayedValues() == null;
+
+        if (items != null) {
+            String[] displayValues = new String[items.size()];
+            for (int i = 0; i < items.size(); i++) {
+                displayValues[i] = items.getString(i);
+            }
+
+            view.setMinValue(0);
+            view.setMaxValue(displayValues.length - 1);
+            view.setDisplayedValues(displayValues);
+        } else {
+            view.setDisplayedValues(null);
         }
 
-        view.setMinValue(0);
-        view.setMaxValue(displayValues.length - 1);
-        view.setDisplayedValues(displayValues);
+        if (updateStaged) {
+            view.updateStagedSelection();
+        }
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = "selected")
     public void setValue(RNNumberPicker view, Integer selected) {
-        view.setValue(selected);
+        if (view.getDisplayedValues() == null) {
+            view.setStagedSelection(selected);
+        } else {
+            view.setValue(selected);
+        }
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = "keyboardInputEnabled")
     public void setKeyboardInputEnabled(RNNumberPicker view, Boolean enabled) {
         view.setKeyboardInputEnabled(enabled);
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = "showDivider")
     public void setShowDivider(RNNumberPicker view, Boolean show) {
         if(!show) {
@@ -64,28 +78,33 @@ public class RNNumberPickerManager extends SimpleViewManager<RNNumberPicker>
         }
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = ViewProps.COLOR, customType = "Color")
     public void setColor(RNNumberPicker view, Integer color) {
         view.setmTextColor(color);
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = ViewProps.FONT_SIZE)
     public void setFontSize(RNNumberPicker view, Integer fontSize) {
         view.setmTextSize(fontSize);
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = ViewProps.FONT_FAMILY)
     public void setFontFamily(RNNumberPicker view, String fontFamily) {
         view.setmFontFamily(fontFamily);
     }
 
+    @SuppressWarnings("unused")
     @ReactProp(name = ViewProps.FONT_WEIGHT)
     public void setFontWeight(RNNumberPicker view, String fontWeight) {
         view.setmFontWeight(fontWeight);
     }
 
     @Override
-    protected void addEventEmitters(final ThemedReactContext reactContext, final RNNumberPicker picker) {
+    protected void addEventEmitters(@NonNull final ThemedReactContext reactContext,
+                                    @NonNull final RNNumberPicker picker) {
         picker.setOnChangeListener(this);
     }
 
